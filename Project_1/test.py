488ac27d402601754b562a888b4cb153c92cdf61
Siju Sakaria <sijus887@gmail.com>
2023-02-19 19:08:05 -0500

    Add test file to read testcases from .txt

--- Project_1/test.py
+++ Project_1/test.py
@@ -1,14 +1,45 @@
-import unittest
-
 from task1 import *
 
 
-class Tests(unittest.TestCase):
-    def test_strategy1_input1(self):
-        days = 5
-        houses = 3
-        schedule = [[1, 3],
-                    [2, 5],
-                    [5, 5]]
-        noOfHouses = count_houses(schedule, days, houses)
-        print("Num of houses painted:", noOfHouses)
+def test_strategy1():
+    with open("test/file1.txt", "r") as file:
+        lines = file.readlines()
+        case = 1
+
+        for i in range(len(lines)):
+            index = 0
+            # check if line is a test case header
+            if lines[i].startswith("# Test case"):
+
+                # extract values for test case
+                n, m = map(int, lines[i + 1].split())
+
+                # initialize 2D array
+                data = [[0] * 2 for _ in range(m)]
+
+                # loop through rows and add to 2D array
+                for j in range(i + 2, i + 2 + m):
+                    row = list(map(int, lines[j].split()))
+                    data[index][0] = row[0]
+                    data[index][1] = row[1]
+                    index += 1
+
+                print("Test case", case)
+                print("n =", n)
+                print("m =", m)
+                print("Houses =", data)
+
+                no_of_houses = count_houses(data, n, m)
+                print("Num of houses painted:", no_of_houses)
+                print()
+
+                case += 1
+                # # move to next test case
+                i = i + m + 2
+
+            else:
+                # move to next line
+                i += 1
+
+if __name__ == '__main__':
+    test_strategy1()
