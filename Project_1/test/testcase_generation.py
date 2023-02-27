367378cd43db7d58d15c827d97fd9fae8b62cfa1
Shreya Shah <shreyashah690@gmail.com>
2023-02-26 19:09:22 -0500

    Updates testcases.

--- Project_1/test/testcase_generation.py
+++ Project_1/test/testcase_generation.py
@@ -1,12 +1,12 @@
 import random
 
 
-def generate_testcase(m, n):
+def generate_testcase(m, n, f):
     available = []
     for i in range(m):
-        start_day = random.randint(1, max(n + 1 - i, 2))
-        j = random.randint(1, m)
-        end_day = random.randint(start_day + 1, start_day + j)
+        start_day = random.randint(0, max(n+1-i, 2))
+        j = random.randint(5, int(m/8))
+        end_day = random.randint(start_day + 1, start_day + f)
         available.append([start_day, min(end_day, n)])
     return available
 
@@ -14,7 +14,8 @@ MAX1 = 100000
 ns = [1000, 2000, 3000, 5000]
 for index, n in enumerate(ns):
     m = int(n/2)
-    a = generate_testcase(m, n)
+    f = 6
+    a = generate_testcase(m, n, f)
     a.sort(key=lambda x: (x[0], x[1]))
     print("# Testcase", index+1)
     print(n, m)
