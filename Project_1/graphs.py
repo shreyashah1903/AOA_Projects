dd26d01c7a8933d7f8a1cb2a3845ee735935d295
Siju Sakaria <sijus887@gmail.com>
2023-02-26 19:51:35 -0500

    Change to histogram

--- Project_1/graphs.py
+++ Project_1/graphs.py
@@ -1,49 +1,39 @@
-# importing the required module
 import matplotlib.pyplot as plt
+import numpy as np
 
-# [500, 1000, 1500, 2500] Days
-# 500, 1000, 1500, 2000, 2500]
-# [496, 979, 1466, 1946, 2426] Strategy 1
-# [480, 955, 1420, 1890, 2353] Strategy 2
-# [498, 975, 1440, 1928, 2402] Strategy 3
-# [500, 995, 1485, 1976, 2467] Strategy 4
-# [500, 995, 1485, 1976, 2467] Strategy 5
-
-# x axis values
+# Input data
 x = [1000, 2000, 3000, 4000, 5000]
-# corresponding y axis values
 y1 = [496, 979, 1466, 1946, 2426]
-
-# plotting the points
-plt.plot(x, y1, label = "Strategy 1")
-
-# corresponding y axis values
 y2 = [480, 955, 1420, 1890, 2353]
-
-plt.plot(x, y2, label = "Strategy 2")
-
-
-# corresponding y axis values
 y3 = [498, 975, 1440, 1928, 2402]
-
-plt.plot(x, y3, label = "Strategy 3")
-
-
-# corresponding y axis values
 y4 = [500, 995, 1485, 1976, 2467]
 
-plt.plot(x, y4, label = "Strategy 4")
+# Create a figure with a single subplot
+fig, ax = plt.subplots()
 
-# naming the x axis
+# Set the bar width
+bar_width = 0.1
+
+# Set the positions of the bars on the x-axis
+bar_positions1 = np.arange(len(x))
+bar_positions2 = [x + bar_width for x in bar_positions1]
+bar_positions3 = [x + bar_width * 2 for x in bar_positions1]
+bar_positions4 = [x + bar_width * 3 for x in bar_positions1]
+
+# Create the bars
+ax.bar(bar_positions1, y1, width=bar_width, label='Strategy 1')
+ax.bar(bar_positions2, y2, width=bar_width, label='Strategy 2')
+ax.bar(bar_positions3, y3, width=bar_width, label='Strategy 3')
+ax.bar(bar_positions4, y4, width=bar_width, label='Strategy 4')
+
+# Add x-axis ticks and labels
+plt.xticks([i + bar_width * 1.5 for i in range(len(x))], x)
+
+# Add labels and legend
 plt.xlabel('Input size(Days)')
-# naming the y axis
 plt.ylabel('Houses painted')
-
-# giving a title to my graph
-plt.title('Experimental study')
-
-# show a legend on the plot
+plt.title('Experimental Study')
 plt.legend()
 
-# function to show the plot
+# Show the plot
 plt.show()
