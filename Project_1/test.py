7346160861235b0528f24379616e3b2016a2a2e3
Shreya Shah <shreyashah690@gmail.com>
2023-02-27 20:20:07 -0500

    Updates testcases.

--- Project_1/test.py
+++ Project_1/test.py
@@ -1,15 +1,32 @@
+import time
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from Project_1.test.testcase_generation import generate_testcase
 from task1 import *
 from task2 import *
 from task3 import *
 from task4 import *
 from task5_bonus import *
 
+N_values = [1000, 2000, 3000, 4000, 5000, 10000]
+
+
 def test_strategies():
-    files = ["test/file2.txt"]
-    for filename in files:
+    files = ["test/testcases_for_n_" + str(x) + ".txt" for x in N_values]
+    for index, filename in enumerate(files):
         with open(filename, "r") as file:
             lines = file.readlines()
             case = 1
+
+            ns = []
+            ms = []
+            s1 = []
+            s2 = []
+            s3 = []
+            s4 = []
+            s5 = []
 
             for i in range(len(lines)):
                 index = 0
@@ -23,6 +40,7 @@ def test_strategies():
                     data = [[0] * 2 for _ in range(m)]
 
                     # loop through rows and add to 2D array
+                    print("{} {} {}".format(file, n, case))
                     for j in range(i + 2, i + 2 + m):
                         row = list(map(int, lines[j].split()))
                         data[index][0] = row[0]
@@ -33,10 +51,13 @@ def test_strategies():
                     print("days =", n)
                     print("houses =", m)
                     print("House availability =", data)
+                    ns.append(n)
+                    ms.append(m)
 
                     print("\nStrategy 1")
                     painted_houses = count_houses_strategy1(data, n, m)
                     print("Num of houses painted:", len(painted_houses))
+                    s1.append(len(painted_houses))
                     for j in range(len(painted_houses)):
                         start, end, day, index = painted_houses[j]
                         print("Day {}, house {} painted with schedule {} {}".format(day, index, start, end))
@@ -44,6 +65,7 @@ def test_strategies():
                     print("\nStrategy 2")
                     painted_houses = count_houses_strategy2(data, n, m)
                     print("Num of houses painted:", len(painted_houses))
+                    s2.append(len(painted_houses))
                     for j in range(len(painted_houses)):
                         start, end, day, index = painted_houses[j]
                         print("Day {}, house {} painted with schedule {} {}".format(day, index, start, end))
@@ -51,6 +73,7 @@ def test_strategies():
                     print("\nStrategy 3")
                     painted_houses = count_houses_strategy3(data, n, m)
                     print("Num of houses painted:", len(painted_houses))
+                    s3.append(len(painted_houses))
                     for j in range(len(painted_houses)):
                         start, end, day, index = painted_houses[j]
                         print("Day {}, house {} painted with schedule {} {}".format(day, index, start, end))
@@ -58,7 +81,7 @@ def test_strategies():
                     print("\nStrategy 4")
                     painted_houses = count_houses_strategy4(data, n, m)
                     print("Num of houses painted:", len(painted_houses))
-
+                    s4.append(len(painted_houses))
                     for j in range(len(painted_houses)):
                         start, end, day, index = painted_houses[j]
                         print("Day {}, house {} painted with schedule {} {}".format(day, index, start, end))
@@ -66,7 +89,7 @@ def test_strategies():
                     print("\nStrategy 5 (Bonus)")
                     painted_houses = count_houses_bonus_strategy(data, n, m)
                     print("Num of houses painted:", len(painted_houses))
-
+                    s5.append(len(painted_houses))
                     for j in range(len(painted_houses)):
                         start, end, day, index = painted_houses[j]
                         print("Day {}, house {} painted with schedule {} {}".format(day, index, start, end))
@@ -81,6 +104,131 @@ def test_strategies():
                     # move to next line
                     i += 1
 
+            plot_graph1(n, ns, ms, s1, s2, s3, s4, s5)
+
+
+def plot_graph1(n, ns, ms, s1, s2, s3, s4, s5):
+    # Create a figure with a single subplot
+    fig, ax = plt.subplots()
+    x = ms
+    bar_width = 0.1
+
+    # Set the positions of the bars on the x-axis
+    bar_positions1 = np.arange(len(x))
+    bar_positions2 = [x + bar_width for x in bar_positions1]
+    bar_positions3 = [x + bar_width * 2 for x in bar_positions1]
+    bar_positions4 = [x + bar_width * 3 for x in bar_positions1]
+
+    # Create the bars
+    ax.bar(bar_positions1, s1, width=bar_width, label='Strategy 1')
+    ax.bar(bar_positions2, s2, width=bar_width, label='Strategy 2')
+    ax.bar(bar_positions3, s3, width=bar_width, label='Strategy 3')
+    ax.bar(bar_positions4, s4, width=bar_width, label='Strategy 4')
+
+    # Add x-axis ticks and labels
+    plt.xticks([i + bar_width * 1.5 for i in range(len(x))], x)
+
+    # Add x-axis ticks and labels
+    plt.xlabel('No of Houses')
+    plt.ylabel('Houses painted')
+    plt.title('Experimental Study for Number of Days (n = {})'.format(n))
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
+
+def plot_graph(n, ns, ms, s1, s2, s3, s4, s5):
+    print("Graph data")
+    print(ns)
+    print(ms)
+    print(s1)
+    print(s2)
+    print(s3)
+    print(s4)
+    print(s5)
+
+    # plt.plot(ns, s1, label="Strategy 1")
+    # plt.plot(ns, s2, label="Strategy 2")
+    # plt.plot(ns, s3, label="Strategy 3")
+    # plt.plot(ns, s4, label="Strategy 4")
+
+    plt.plot(ms, s1, label="Strategy 1")
+    plt.plot(ms, s2, label="Strategy 2")
+    plt.plot(ms, s3, label="Strategy 3")
+    plt.plot(ms, s4, label="Strategy 4")
+
+    # naming the x axis
+    plt.xlabel('No of houses')
+    # naming the y axis
+    plt.ylabel('Houses painted')
+
+    # giving a title to my graph
+    plt.title('Experimental study (n={})'.format(n))
+
+    # show a legend on the plot
+    plt.legend()
+
+    # function to show the plot
+    plt.show()
+
+
+def comparative_study_bonus():
+    for n in [10 ** 6, 10 ** 5, 10 ** 4, 10 ** 3, 10 ** 2]:
+        ms = []
+        s4 = []
+        s5 = []
+        for m in [int(n / 8), int(n / 4), int(n / 2), n]:
+            ms.append(m)
+            schedule = generate_testcase(m, n, 6)
+            schedule.sort(key=lambda x: (x[0], x[1]))
+
+            # Strategy 5
+            start_time = time.time()
+            _ = count_houses_bonus_strategy(schedule, n, m)
+            end_time = time.time()
+            time_taken = end_time - start_time
+            s5.append(time_taken)
+
+            # Strategy 4
+            start_time = time.time()
+            _ = count_houses_strategy4(schedule, n, m)
+            end_time = time.time()
+            time_taken = end_time - start_time
+            s4.append(time_taken)
+
+
+
+        plot_graph_running_time(n, ms, s4, s5)
+
+
+def plot_graph_running_time(n, ms, s4, s5):
+    # Create a figure with a single subplot
+    fig, ax = plt.subplots()
+    x = ms
+    bar_width = 0.2
+
+    # Set the positions of the bars on the x-axis
+    bar_positions1 = np.arange(len(x))
+    bar_positions2 = [x + bar_width for x in bar_positions1]
+
+    # Create the bars
+    ax.bar(bar_positions1, s4, width=bar_width, label='Strategy 4 (Theta(n + mlogm)')
+    ax.bar(bar_positions2, s5, width=bar_width, label='Strategy 5 (Theta(mlogm))')
+
+    # Add x-axis ticks and labels
+    plt.xticks([i + bar_width * 1.5 for i in range(len(x))], x)
+
+    # Add x-axis ticks and labels
+    plt.xlabel('No of Houses')
+    plt.ylabel('Time taken to run')
+    plt.title('Experimental Study for Number of Days (n = {})'.format(n))
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
 
 if __name__ == '__main__':
-    test_strategies()
+    # test_strategies()
+    comparative_study_bonus()
--- Project_1/test/testcase_generation.py
+++ Project_1/test/testcase_generation.py
@@ -5,20 +5,28 @@ def generate_testcase(m, n, f):
     available = []
     for i in range(m):
         start_day = random.randint(0, max(n+1-i, 2))
-        j = random.randint(5, int(m/8))
-        end_day = random.randint(start_day + 1, start_day + f)
-        available.append([start_day, min(end_day, n)])
+        j = random.randint(1, int(m/4))
+        end_day = random.randint(start_day + 1, start_day + j)
+        available.append([start_day, min(end_day, 10001)])
     return available
 
+# m = 5000
+f = 6
+
 MAX1 = 100000
-ns = [1000, 2000, 3000, 5000]
+ns = [1000, 2000, 3000, 4000, 5000, 10000]
 for index, n in enumerate(ns):
-    m = int(n/2)
-    f = 6
-    a = generate_testcase(m, n, f)
-    a.sort(key=lambda x: (x[0], x[1]))
-    print("# Testcase", index+1)
-    print(n, m)
-    for ar in a:
-        print(ar[0], ar[1])
-    print()
+    i = 0
+    filename = 'testcases_for_n_' + str(n) + '.txt'
+    file = open(filename, 'w')
+    for m in [int(n/16), int(n/8), int(n/4), int(n/2), n]:
+        a = generate_testcase(m, n, f)
+        a.sort(key=lambda x: (x[0], x[1]))
+        file.write("# Testcase {}\n".format(str(i+1)))
+        file.write("{} {}\n".format(n, m))
+        for ar in a:
+            file.write("{} {}\n".format(ar[0], ar[1]))
+        # file.write("\n")
+        i += 1
+    file.close()
+
