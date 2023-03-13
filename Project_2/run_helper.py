398571a59a31f9b2d5ff5be298f9cbcf5f2f74ca
Shreya Shah <shreyashah690@gmail.com>
2023-03-13 12:19:22 -0700

    Implemented Task 1 for Project 2.

---
+++ Project_2/run_helper.py
@@ -0,0 +1,10 @@
+def fetch_input():
+    m, n, h = map(int, input().split())
+    p = []
+    for i in range(m):
+        row = list(map(int, input().split()))
+        if len(row) != n:
+            print("Error: Each row should have {} integers.".format(n))
+            return None
+        p.append(row)
+    return m, n, h, p
---
+++ Project_2/task1.py
@@ -0,0 +1,34 @@
+# Problem1: Given a matrix p of m ×n integers (non-negative) representing the minimum number of
+# trees that must be planted on each plot and an integer h (positive), find the bounding
+# indices of a square area where each plot enclosed requires a minimum of h trees to be
+# planted.
+
+# Task1: Design a Θ(m^3*n^3) time Brute Force algorithm for solving Problem1.
+
+from run_helper import fetch_input
+
+
+def find_square_area(m, n, h, matrix):
+    x1, y1, x2, y2 = -1, -1, -1, -1
+    maximum_size = 0
+    for i in range(m):
+        for j in range(n):
+            for square_size in range(1, min(m - i + 1, n - j + 1)):
+                valid_area = True
+                for row in range(i, i + square_size):
+                    for col in range(j, j + square_size):
+                        if matrix[row][col] < h:
+                            valid_area = False
+                            break
+                    if not valid_area:
+                        break
+
+                if valid_area and square_size > maximum_size:
+                    maximum_size = square_size
+                    x1, y1, x2, y2 = i, j, i+square_size-1, j+square_size-1
+    return x1, y1, x2, y2
+
+
+m, n, h, p = fetch_input()
+x1, y1, x2, y2 = find_square_area(m, n, h, p)
+print(x1, y1, x2, y2)
