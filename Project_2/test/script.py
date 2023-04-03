c242795ec6dfeb9f66291073737cb525e3c61a36
Siju Sakaria <sijus887@gmail.com>
2023-04-02 20:11:49 -0400

    Fix script

--- Project_2/test/script.py
+++ Project_2/test/script.py
@@ -9,18 +9,30 @@ gen_dir = 'gen'
 # Get a list of all the input files
 input_files = os.listdir(input_dir)
 
-def checkFiles(file_regex, task_list):
-    # Loop through each input file
+tasks = {'p1': ['../task1.py', '../task2.py', '../task3.py'], 'p2': ['../task4.py', '../task5A.py', '../task5B.py'],
+         'p3': ['../task7A.py', '../task7B.py']}
 
-    fail_count = 0
-    total_count = 0
-    for taskFile in task_list:
-        for filename in input_files:
+
+def checkFiles():
+    cleanup()
+    for filename in input_files:
+        total_count = 0
+        fail_count = 0
+
+        # Construct the input and output file paths
+        input_path = os.path.join(input_dir, filename)
+        output_path = os.path.join(output_dir, filename)
+        gen_path = os.path.join(gen_dir, filename)
+
+        if 'p1' in filename:
+            task_list = tasks['p1']
+        elif 'p2' in filename:
+            task_list = tasks['p2']
+        else:
+            task_list = tasks['p3']
+
+        for taskFile in task_list:
             total_count += 1
-            # Construct the input and output file paths
-            input_path = os.path.join(input_dir, filename)
-            output_path = os.path.join(output_dir, filename)
-            gen_path = os.path.join(gen_dir, filename)
 
             # Run p1.py on the input file and save the output to the gen directory
             with open(input_path, 'r') as f_in, open(gen_path, 'w') as f_gen:
@@ -29,11 +41,20 @@ def checkFiles(file_regex, task_list):
             # Compare the generated output to the expected output
             with open(output_path, 'r') as f_out, open(gen_path, 'r') as f_gen:
                 expected_output = f_out.read()
-                generated_output = f_gen.read()
+                generated_output = f_gen.read().strip()
                 if expected_output == generated_output:
                     print(f'{filename} - {taskFile}: PASS')
                 else:
                     print(f'{filename} - {taskFile}: FAIL')
                     fail_count += 1
 
-    print(f'{fail_count} failed out of {total_count}')
+        print(f'{fail_count} failed out of {total_count}')
+
+
+def cleanup():
+    dir = 'gen'
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+
+if __name__ == '__main__':
+    checkFiles()
